efd90e879ea2ae62d58a6ca6ac121dea40e34bbc
Ran Tian <tianrandesign@gmail.com>
2020-07-10 20:51:15 -0400

    envelope
    envelope

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById("lottie"),
+  renderer: "svg",
+  loop: true,
+  autoplay: false,
+  path: "data.json"
+	
+});
+
+/* Play&Stop by Single click/mouse over*/
+/* Use this code below if by Single click*/
+//
+function start() {
+	"use strict";
+    animation.play();
+	document.getElementById("playbtn").style.display = "none";
+	document.getElementById("stopbtn").style.display = "inline";
+}
+document.querySelector('.start').addEventListener('click', start);
+
+function stop() {
+	"use strict";
+    animation.pause();
+	document.getElementById("stopbtn").style.display = "none";
+	document.getElementById("playbtn").style.display = "inline";
+}
+document.querySelector('.stop').addEventListener('click', stop);
+
+/* Use this code below if by mouse over and leave*/
+/*document.getElementById('lottie').addEventListener('mouseover', function(){ animation.play(); })
+document.getElementById('lottie').addEventListener('mouseleave', function(){ animation.stop(); });*/
